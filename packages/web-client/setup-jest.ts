7e1349b68f2d7c7854776e3cf9c141a3b948519d
ocknamo <s14pes@gmail.com>
2025-03-12 12:18:00 +0900

    fix(web-client): setup-jest.tsの警告を修正

--- packages/web-client/setup-jest.ts
+++ packages/web-client/setup-jest.ts
@@ -1,4 +1,6 @@
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
 
 // Global mocks for testing
 Object.defineProperty(global, 'window', { value: global });
